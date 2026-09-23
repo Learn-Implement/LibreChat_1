9eb774b9b2bd6b9b56ca963b5a69c2fcc3729ec7
Learn-Implement <294831306+Learn-Implement@users.noreply.github.com>
2026-09-23 12:33:28 +0300

    Reset handle.js to upstream version (patch no longer needed)

--- api/server/services/Runs/handle.js
+++ api/server/services/Runs/handle.js
@@ -65,7 +65,7 @@ async function waitForRun({
   thread_id,
   runManager,
   pollIntervalMs = 2000,
-  timeout = 60000 * 15,   // 15 minutes,
+  timeout = 60000 * 3,
 }) {
   let timeElapsed = 0;
   let run;
@@ -234,7 +234,7 @@ async function _handleRun({ openai, run_id, thread_id }) {
     thread_id,
     runManager,
     pollIntervalMs: 2000,
-    timeout: 60000 * 15,   // 15 minutes
+    timeout: 60000,
   });
   const actions = [];
   if (run.required_action) {
